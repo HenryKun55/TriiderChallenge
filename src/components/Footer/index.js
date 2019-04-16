dbe5ffd0a04a924385078438935c633fbd8cc0d6
Flávio <henriqueflavio55@gmail.com>
2019-04-16 12:24:53 -0300

    Adicionando primeira parte do footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+import Nav from 'react-bootstrap/Nav'
 
 import logo from '../../assets/logo.png'
 
@@ -8,7 +9,16 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="contentFooter">
-        <img className="logoFooter" width="80" height="20" alt="Triider Logo" src={logo}></img>
+        <div className="footerLeft">
+          <img className="logoFooter" width="80" height="20" alt="Triider Logo" src={logo}></img>
+        </div>
+        <div className="footerRight" >
+          <Nav className="footerNavRight">
+            <Nav.Link className="linkFooter" >Central de Ajuda</Nav.Link>
+            <Nav.Link className="linkFooter" >Termos de uso</Nav.Link>
+            <Nav.Link className="linkFooter" >Código de conduta</Nav.Link>
+          </Nav>
+        </div>
       </div>
     </footer>
   );
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -35,7 +35,7 @@ export default class Main extends Component {
             </div>
             <Container fluid={true} >
                 <Row>
-                    <Col>
+                    <Col sm>
                         <Card bsPrefix="card">
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
@@ -48,7 +48,7 @@ export default class Main extends Component {
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col>
+                    <Col sm>
                         <Card bsPrefix="card">
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
@@ -61,48 +61,7 @@ export default class Main extends Component {
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col>
-                        <Card bsPrefix="card">
-                            <Card.Img variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Card bsPrefix="card">
-                            <Card.Img variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card bsPrefix="card">
-                            <Card.Img variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
+                    <Col sm>
                         <Card bsPrefix="card">
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
@@ -117,7 +76,7 @@ export default class Main extends Component {
                     </Col>
                 </Row>
                 <Row>
-                    <Col>
+                    <Col sm>
                         <Card bsPrefix="card">
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
@@ -130,7 +89,7 @@ export default class Main extends Component {
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col>
+                    <Col sm>
                         <Card bsPrefix="card">
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
@@ -143,7 +102,48 @@ export default class Main extends Component {
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col>
+                    <Col sm>
+                        <Card bsPrefix="card">
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm>
+                        <Card bsPrefix="card">
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm>
+                        <Card bsPrefix="card">
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm>
                         <Card bsPrefix="card">
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
