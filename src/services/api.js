62eca1fb2609540793fb434e99ee4cda17ef1f62
Flávio <henriqueflavio55@gmail.com>
2019-04-19 21:57:49 -0300

    Trocando Base Url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://5c5d8680ef282f0014c3d956.mockapi.io/api/v1'
+    baseURL: 'https://5c5d8680ef282f0014c3d956.mockapi.io/api/v1'
 })
 
 export default api;
